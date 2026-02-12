988895db144fe2c30cefb6319025b80f4acf212f
elseitovalisher-user <elseitov.alisher@gmail.com>
2026-02-12 23:28:19 +0500

    First commit Homework Lesson 5

---
+++ homework.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello")
+
+	//Task 1
+	var schooling int = 11
+
+	fmt.Println("Studied", schooling)
+	schooling = 12
+	fmt.Println("Studied", schooling)
+
+	fmt.Println("Task 2")
+	var name string
+	name = "Vladislav"
+	fmt.Print(name)
+	name = " Alisher"
+	fmt.Println(name)
+
+	fmt.Println("Task 3")
+	var steps int
+	steps = 0
+	fmt.Println("morning steps: ", steps)
+	steps = 2000
+	fmt.Println("afternoon steps: ", steps)
+	fmt.Println("Хорошая работа! Вы уже на пути к своей ежедневной цели\n",
+		"Well done")
+	var largeNumber int
+	largeNumber = 5000055
+	fmt.Println("Task 4", largeNumber)
+
+	const breakTime = 15
+	fmt.Println("task 5", breakTime)
+	const breakTime = 20
+	fmt.Println(breakTime) // т.к. число является переменным константа, его невозможно менять в последующем коде
+
+}
--- package main.go
+++ package main.go
@@ -1,9 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
-func main() {
-	fmt.Println("Hello My friend ")
-	fmt.Println("second Commit")
-	
+func main1() {
+	/*	var name string
+		name = "Alisher"
+		fmt.Println(name)
+
+		age:= 25
+		println(age)
+
+		name2:="AAA"
+		age2:= age
+		fmt.Println(name2, age2)
+
+		age = 50
+		age2= 55
+		fmt.Println(age, age2)
+
+		const pi = 3.14
+		fmt.Println(pi) */
+
+	// var schooling int = 11
+
+	// fmt.Println("Studied",schooling)
+	// schooling = 12
+	// fmt.Println("Studied", schooling)
+	// var pi int
+	// pi = 3
+	// fmt.Println("The value of the pi is", pi)
+	// pi2 := float64(pi)
+	// fmt.Println("the value of the pi2 is:", pi2)
+	// pi2 = 3.14
+	// fmt.Println("the updated value of the pi2 is:", pi2)
+	a := "10"
+	b, _ := strconv.Atoi(a)
+	b = b+5
+	fmt.Println(b)
 }
